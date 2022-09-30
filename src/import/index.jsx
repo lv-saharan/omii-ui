9f20ad97a9ea95b220768786eb83389bd1712bf8
lv-saharan <saharan@foxmail.com>
2022-09-30 17:11:30 +0800

    version =1.0.2

--- src/import/index.jsx
+++ src/import/index.jsx
@@ -1,40 +1,42 @@
 const { h, classNames, uniqueTag } = omii;
 import uiBase from "../uiBase";
-import css from "./index.scss"
+import css from "./index.scss";
 export default class extends uiBase {
   static css = [css];
   static defaultProps = {
     src: null,
     module: "default",
-    loading: <oi-loading size="6" />
+    loading: <oi-loading size="6" />,
   };
   static propTypes = {
     src: String,
-    module: String
+    module: String,
   };
 
-  #module
+  #module;
 
   #load() {
-    let { src, module, loading } = this.$props
-    this.#module = loading
+    let { src, module, loading } = this.$props;
+    this.#module = loading;
     if (src && module) {
-      import(src).then(modules => {
-        let $module = modules[module]
-        if ($module instanceof HTMLElement) {
-          this.#module = uniqueTag($module)
-        } else {
-          this.#module = $module
-        }
-        this.updateSelf()
-      }).catch(exc => {
-        console.error("import module error！", src, module, exc)
-      })
+      import(src)
+        .then((modules) => {
+          let $module = modules[module];
+          if (typeof $module === "function") {
+            const App = uniqueTag($module);
+            this.#module = <App />;
+          } else {
+            this.#module = $module;
+          }
+          this.updateSelf();
+        })
+        .catch((exc) => {
+          console.error("import module error！", src, module, exc);
+        });
     }
-
   }
   install() {
-    this.#load()
+    this.#load();
   }
   render(props) {
     return this.#module;
